ad1d31dcd67acb858f8755541ff839dc5de9ac1b
Alyx-Clark <alexclark.edu@gmail.com>
2022-01-07 15:46:36 -0500

    finish biggest_cast

--- skeleton/movie_buff/01_queries.rb
+++ skeleton/movie_buff/01_queries.rb
@@ -43,7 +43,12 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the
   # largest casts (i.e most actors)
-
+  Movie 
+    .select(:id, :title)
+    .joins(:actors)
+    .group("movies.id")
+    .order('COUNT(actor_id) DESC')
+    .limit(3)
 end
 
 def directed_by_one_of(them)
